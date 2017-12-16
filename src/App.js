b62379bb3673c58a01c7363559b8db53476d8ffa
wemmm <jenny.wem@gmail.com>
2017-12-16 14:39:10 +0000

    Adds existing files

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import customers from './customers.json';
+import './styles.scss';
+
+const CustomerRow = ({customer}) => (
+  <tr>
+    <td>{customer.title} {customer.first_name} {customer.last_name}</td>
+    <td>{customer.profession}</td>
+    <td>{customer.has_children ? "Y" : "N"}</td>
+    <td>
+      {customer.dominant_traits[0].level}
+      {customer.dominant_traits[0].primary_trait},
+      {customer.dominant_traits[1].level}
+      {customer.dominant_traits[1].secondary_trait},
+    </td>
+  </tr>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <h1>Walnut</h1>
+        </header>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Profession</th>
+              <th>Children</th>
+              <th>Traits</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            customers.slice(0, 10).map(c => <CustomerRow customer={c}/>)
+          }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
