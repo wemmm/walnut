7098475c91dd7dfa17e9ae4a9518fdebe95ffe4b
wemmm <jenny.wem@gmail.com>
2017-12-17 17:35:53 +0000

    Styling
    - Adds react-bootstrap
    - Adds icons from react-icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Button, ButtonGroup, Table } from 'react-bootstrap';
 import customers from './customers.json';
-import './styles.scss';
+import { FaUser, FaBriefcase, FaChild, FaSmileO, FaMoney, FaCheck, FaArrowUp, FaArrowDown } from 'react-icons/lib/fa';
 
 const CustomerRow = ({customer}) => (
   <tr>
     <td>{customer.title} {customer.first_name} {customer.last_name}</td>
     <td>{customer.profession}</td>
-    <td>{customer.has_children ? "Y" : "N"}</td>
+    <td>{customer.has_children ? <FaCheck/> : ""}</td>
     <td>
       {customer.dominant_traits[0].level} {customer.dominant_traits[0].primary_trait}, {customer.dominant_traits[1].level} {customer.dominant_traits[1].secondary_trait}
     </td>
@@ -40,14 +41,14 @@ class App extends Component {
         <header>
           <h1>Walnut</h1>
         </header>
-        <table>
+        <Table striped>
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Profession</th>
-              <th>Children</th>
-              <th>Traits</th>
-              <th>Income</th>
+              <th><FaUser/> Name</th>
+              <th><FaBriefcase/> Profession</th>
+              <th><FaChild/> Children</th>
+              <th><FaSmileO/> Traits</th>
+              <th><FaMoney/> Income</th>
             </tr>
           </thead>
           <tbody>
@@ -55,15 +56,17 @@ class App extends Component {
             customers.slice(0, this.state.recordsShown).map(c => <CustomerRow customer={c}/>)
           }
           </tbody>
-        </table>
+        </Table>
 
-        <button onClick={this.incrementRecords}>
-          Next 10
-        </button>
+        <ButtonGroup>
+        <Button bsStyle="primary" onClick={this.incrementRecords}>
+          <FaArrowDown/>
+        </Button>
 
-        <button onClick={this.decrementRecords}>
-          Previous 10
-        </button>
+        <Button bsStyle="primary" onClick={this.decrementRecords}>
+          <FaArrowUp/>
+        </Button>
+        </ButtonGroup>
 
       </div>
     );
