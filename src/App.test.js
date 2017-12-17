67579340341778ff0e7f8309e7bacc94f6af55f2
wemmm <jenny.wem@gmail.com>
2017-12-17 13:38:17 +0000

    Configures shallow renderer tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<App />);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+  });
+
+  it('has a header', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<App />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.children[0].type).toBe('header');
+  });
+
+  it('has a button to expand the table', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<App />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.children[2].type).toBe('button');
+  });
+
+  it('has a button to collapse the table', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<App />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.children[3].type).toBe('button');
+  });
 });
