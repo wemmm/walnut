7d34e1a6411a97c40950aabe3f4a8d98d645cc4f
wemmm <jenny.wem@gmail.com>
2018-01-08 16:34:53 +0000

    Adds widowed and unknown to marital status pie chart

--- src/Chart.js
+++ src/Chart.js
@@ -5,7 +5,9 @@ import { PieChart, Pie, Legend, Tooltip } from 'recharts';
 const data01 = [{name: 'Divorced', value: (customers.filter(c => c.marital_status == "Divorced").length)},
                 {name: 'Married', value: (customers.filter(c => c.marital_status == "Married").length)},
                 {name: 'Single', value: (customers.filter(c => c.marital_status == "Single").length)},
-                {name: 'Other', value: (customers.filter(c => c.marital_status == "Other").length)}]
+                {name: 'Widowed', value: (customers.filter(c => c.marital_status == "Widowed").length)},
+                {name: 'Other', value: (customers.filter(c => c.marital_status == "Other").length)},
+                {name: 'Unknown', value: (customers.filter(c => c.marital_status == null).length)}]
 
 const data02 = [{name: 'Red Bank', value: (customers.filter(c => c.company == "Red Bank").length)},
                 {name: 'Blue Bank', value: (customers.filter(c => c.company == "Blue Bank").length)},
